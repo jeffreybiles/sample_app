8d8cddfe037a1e22f253edf8e95165cfbdba4c46
jeffreybiles <bilesjeffrey@gmail.com>
2011-11-20 20:57:49 -0600

    Initial commit, but without webrat gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'b6ea624ac4de5d846829500bd491bc467d86bab14c3fd5250c888ae2c3928942e0f46558f78edc809c495dee87411572a4efeef34110b902aad81903cf234000'
