d033e33775fcf2d09597e2456eb1ec309e553811
jeffreybiles <bilesjeffrey@gmail.com>
2011-11-23 17:13:31 -0600

    Done with static pages

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ end
 group :test do
 	gem 'rspec'
 	#gem 'webrat', '0.7.1'
+	gem 'spork'
 end
 
 # Gems used only for assets and not required
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,13 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
-require 'rspec/autorun'
+require 'rubygems'
+require 'spork'
 
+Spork.prefork do
+  # Loading more in this block will cause your tests to run faster. However, 
+  # if you change any configuration or code from libraries loaded here, you'll
+  # need to restart spork for it take effect.
+	ENV["RAILS_ENV"] ||= 'test'
+		require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
@@ -31,3 +35,8 @@ RSpec.configure do |config|
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
 end
+end
+
+Spork.each_run do
+end
+
